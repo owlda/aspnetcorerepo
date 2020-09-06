a26ecdbceefba460269613e12c0ac9e5fd9cb2ab
owlda <ad28dec@gmail.com>
2020-09-06 09:10:49 -0400

    Constrains added with DataAnnotations

--- Migrations/ModelsDBContextModelSnapshot.cs
+++ Migrations/ModelsDBContextModelSnapshot.cs
@@ -26,7 +26,9 @@ namespace apsnetproject.Migrations
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
                     b.Property<string>("Name")
-                        .HasColumnType("nvarchar(max)");
+                        .IsRequired()
+                        .HasColumnType("nvarchar(255)")
+                        .HasMaxLength(255);
 
                     b.Property<int>("OfficeId")
                         .HasColumnType("int");
@@ -49,13 +51,15 @@ namespace apsnetproject.Migrations
                         .HasColumnType("int");
 
                     b.Property<string>("Name")
-                        .HasColumnType("nvarchar(max)");
+                        .IsRequired()
+                        .HasColumnType("nvarchar(255)")
+                        .HasMaxLength(255);
 
                     b.HasKey("Id");
 
                     b.HasIndex("MakeId");
 
-                    b.ToTable("Model");
+                    b.ToTable("Models");
                 });
 
             modelBuilder.Entity("apsnetproject.Models.Office", b =>
@@ -66,10 +70,14 @@ namespace apsnetproject.Migrations
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
                     b.Property<string>("Location")
-                        .HasColumnType("nvarchar(max)");
+                        .IsRequired()
+                        .HasColumnType("nvarchar(255)")
+                        .HasMaxLength(255);
 
                     b.Property<string>("Name")
-                        .HasColumnType("nvarchar(max)");
+                        .IsRequired()
+                        .HasColumnType("nvarchar(255)")
+                        .HasMaxLength(255);
 
                     b.HasKey("Id");
 
--- Models/Office.cs
+++ Models/Office.cs
@@ -1,15 +1,21 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace apsnetproject.Models
 {
+    [Table("Offices")]
     public class Office
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
-
+        [Required]
+        [StringLength(255)]
         public string Location { get; set; }
 
         public ICollection<Make> Makes { get; set; }
