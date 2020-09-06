800ccb723ad8585e998774f0233232012db993ed
owlda <ad28dec@gmail.com>
2020-09-06 10:30:54 -0400

    Controllers added to project to populate data

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace apsnetproject
 {
@@ -27,10 +28,14 @@ namespace apsnetproject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+          
             services.AddDbContext<ModelsDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
 
             services.AddControllers();
+            object p = services.AddMvc(option => option.EnableEndpointRouting = false)
+                               .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+                               .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+          
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
